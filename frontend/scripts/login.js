2b7d12a5288e1afa4883b986249b4c59919c45ae
Mary Polish <polishchuk.mariyatv21@gmail.com>
2024-12-21 16:59:18 +0200

    login.js

--- frontend/scripts/login.js
+++ frontend/scripts/login.js
@@ -1,24 +1,26 @@
-document.getElementById('loginForm').addEventListener('submit', async (e) => {
-    e.preventDefault();
-    
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('loginForm').addEventListener('submit', async (e) => {
+        e.preventDefault();
+        
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
 
-    const response = await fetch('http://localhost:5500/api/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
+        const response = await fetch('http://localhost:5500/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, password }),
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            localStorage.setItem('token', data.token);
+            alert('Успішний вхід');
+            window.location.href = 'calendar.html'; // перенаправлення на головну сторінку
+        } else {
+            alert(data.message || 'Помилка входу');
+        }
     });
-
-    const data = await response.json();
-
-    if (response.ok) {
-        localStorage.setItem('token', data.token);
-        alert('Успішний вхід');
-        window.location.href = 'calendar.html'; // перенаправлення на головну сторінку
-    } else {
-        alert(data.message || 'Помилка входу');
-    }
 });
